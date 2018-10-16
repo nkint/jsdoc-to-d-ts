40ad0f085143200ddb02c94863eba66506718cbe
alberto <alberto.massa@ymail.com>
2018-10-16 16:41:03 +0200

    handle case with brackets that seems not well supported by doctrine

--- index.js
+++ index.js
@@ -5,23 +5,24 @@ const extract = require('extract-comments')
 const espree = require('espree')
 
 const namedir = 'gl-vec3'
-const files = fs.readdirSync(path.join('.', namedir)).filter(name => {
-  const extension = path.extname(name)
-  return extension === '.js'
-})
+const files = fs
+  .readdirSync(path.join('.', namedir))
+  .filter(name => {
+    const extension = path.extname(name)
+    return extension === '.js'
+  })
+  .filter(name => name !== 'index.js')
 
 function toSignature(file, originalRedirect = null) {
-  // console.log(`\ntoSignature -> file: ${file}, originalRedirect: ${originalRedirect}`)
   const filename = path.join('.', namedir, file)
   const filecontent = fs.readFileSync(filename, 'utf-8')
 
   const astCode = espree.parse(filecontent)
 
-  // check if contains only a require
+  // check if contains only a require and redirect to a different file
   const isAssignment = astCode.body[0].expression.right.type === 'CallExpression'
   if (isAssignment && astCode.body[0].expression.right.callee.name === 'require') {
     const arg = astCode.body[0].expression.right.arguments[0].value
-    // console.log(`${filename} is a redirect`)
     const redirectFile = `${path.basename(arg)}.js`
     return toSignature(redirectFile, file)
   }
@@ -30,8 +31,10 @@ function toSignature(file, originalRedirect = null) {
   if (commentObject) {
     try {
       const comment = extract(filecontent)[0].value
+      // seems that doctrine can not handle params like [arg] so remove `[]`
+      const commentNoBrackets = comment.replace(/((\[\s*)|(\s*\]))/g, '')
       const functionname = path.basename(originalRedirect || file, '.js')
-      const astComment = doctrine.parse([comment].join('\n'))
+      const astComment = doctrine.parse([commentNoBrackets].join('\n'))
       const params = astComment.tags.filter(param => param.title === 'param')
       const returns = astComment.tags.filter(param => param.title === 'returns')[0]
       const signature = `export function ${functionname}(${params
@@ -50,10 +53,12 @@ function toSignature(file, originalRedirect = null) {
 }
 
 let moduleString = `declare module 'gl-vec3' {` + '\n\n'
-files.forEach(filename => {
-  const signature = toSignature(filename)
-  moduleString += signature
-})
+files
+  // .slice(16, 17)
+  .forEach(filename => {
+    const signature = toSignature(filename)
+    moduleString += signature
+  })
 moduleString += '\n}'
 
 // console.log(moduleString)
